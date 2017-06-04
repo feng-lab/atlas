3516c58f98c77116ffd65230b28cde37f902b83d
Linqing Feng <fenglinqing@gmail.com>
2017-06-04 21:29:52 +0900

    use external folder for ippiw

--- util/build_ext_libs.py
+++ util/build_ext_libs.py
@@ -23,7 +23,8 @@ def get_package_top_level_folder(file: str, folder: str):
     if file.lower().endswith('.zip'):
         with zipfile.ZipFile(file, mode='r') as zf:
             return os.path.join(folder, os.path.commonprefix(zf.namelist()))
-    elif file.lower().endswith('.tar.gz') or file.lower().endswith('.tar.bz2') or file.lower().endswith('.tar.xz'):
+    elif file.lower().endswith('.tar.gz') or file.lower().endswith('.tar.bz2') or file.lower().endswith('.tar.xz') \
+            or file.lower().endswith('.tgz'):
         with tarfile.open(file, mode='r|*') as tf:
             return os.path.join(folder, os.path.commonprefix(tf.getnames()))
     elif file.lower().endswith('.7z'):
@@ -35,7 +36,8 @@ def unpack_file_to_folder(file: str, folder: str):
     if file.lower().endswith('.zip'):
         with zipfile.ZipFile(file, mode='r') as zf:
             zf.extractall(path=folder)
-    elif file.lower().endswith('.tar.gz') or file.lower().endswith('.tar.bz2') or file.lower().endswith('.tar.xz'):
+    elif file.lower().endswith('.tar.gz') or file.lower().endswith('.tar.bz2') or file.lower().endswith('.tar.xz') \
+            or file.lower().endswith('.tgz'):
         with tarfile.open(file, mode='r|*') as tf:
             tf.extractall(path=folder)
     elif file.lower().endswith('.7z'):
@@ -813,7 +815,7 @@ def build_vtk(src_dir: str, install_dir: str, ext_dir: str):
         shutil.rmtree(build_dir, ignore_errors=False)
 
 
-def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str, ext_dir: str):
+def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str, ext_dir: str, ippiw_dir: str):
     build_dir = create_build_dir(src_dir)
     shutil.rmtree(install_dir, ignore_errors=True)
 
@@ -840,7 +842,7 @@ def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str, ext_dir: 
 
             cmakecmd = get_cmake_cmd_common_part(install_dir)
             cmakecmd.extend(['-DIPPROOT=' + os.environ['ICPP_COMPILER17'] + 'ipp',
-                             '-DIPPIWROOT=' + os.environ['ICPP_COMPILER17'] + 'ipp',
+                             '-DIPPIWROOT=' + ippiw_dir,
                              '-DBUILD_IPP_IW:BOOL=OFF',
                              '-DBUILD_WITH_DYNAMIC_IPP:BOOL=OFF',
                              '-DBUILD_WITH_STATIC_CRT:BOOL=OFF',
@@ -908,7 +910,7 @@ def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str, ext_dir: 
 
             cmakecmd = get_cmake_cmd_common_part(install_dir)
             cmakecmd.extend(['-DIPPROOT=/opt/intel/ipp',
-                             '-DIPPIWROOT=/opt/intel/ipp',
+                             '-DIPPIWROOT=' + ippiw_dir,
                              '-DBUILD_IPP_IW:BOOL=OFF',
                              '-DBUILD_WITH_DYNAMIC_IPP:BOOL=OFF',
                              '-DWITH_PTHREADS_PF:BOOL=OFF',
@@ -969,9 +971,9 @@ def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str, ext_dir: 
                 to_lines = []
                 for line in from_lines:
                     line = line.replace(r';tbb;ippiw;ippcv;ippi;ippcc;ipps;ippvm;ippcore',
-                                        r'')
+                                        r';ippiw')
                     line = line.replace(r'tbb;ippiw;ippcv;ippi;ippcc;ipps;ippvm;ippcore;',
-                                        r'')
+                                        r'ippiw;')
                     f.write(line)
                     to_lines.append(line)
             print(''.join(list(difflib.unified_diff(from_lines, to_lines, fromfile=orig_file_2, tofile='<new>'))))
@@ -1150,7 +1152,22 @@ def build_libs(libs: dict, update_src: bool):
             update_or_clone_git_repository(src_contrib_dir, 'git@github.com:Itseez/opencv_contrib.git')
         assert os.path.exists(src_dir)
         assert os.path.exists(src_contrib_dir)
-        build_opencv(src_dir, src_contrib_dir, os.path.join(ext_dir, 'opencv'), ext_dir)
+        if sys.platform.startswith('win32'):
+            ippiw_package_name = find_src_package_with_glob(os.path.join(src_package_dir, 'w_ippiw*'))
+        else:
+            ippiw_package_name = find_src_package_with_glob(os.path.join(src_package_dir, 'm_ippiw*'))
+        ippiw_package_unpack_folder = get_package_top_level_folder(ippiw_package_name, base_dir)
+        if update_src:
+            shutil.rmtree(ippiw_package_unpack_folder, ignore_errors=True)
+            unpack_file_to_folder(ippiw_package_name, base_dir)
+        if sys.platform.startswith('win32'):
+            ippiw_dir = ippiw_package_unpack_folder + '\\ippiw_2017_windows'
+        else:
+            glob_copy(ippiw_package_unpack_folder + '/ippiw_2017_osx/lib/*.a',
+                      ippiw_package_unpack_folder + '/ippiw_2017_osx/lib/intel64/')
+            ippiw_dir = ippiw_package_unpack_folder + '/ippiw_2017_osx'
+        assert os.path.exists(ippiw_dir)
+        build_opencv(src_dir, src_contrib_dir, os.path.join(ext_dir, 'opencv'), ext_dir, ippiw_dir)
 
     if libs['botan']:
         src_dir = os.path.join(base_dir, 'botan')
