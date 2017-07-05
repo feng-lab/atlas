e069ce8bbd15fe7bc459a9b66bb9f438ca206207
Linqing Feng <fenglinqing@gmail.com>
2017-07-05 18:58:55 +0900

    build on windows except one ICE, still need to 1) fix qglobal.h regarding qOverload 2) rebuild ipp_iw with msvc2017

--- src/3rdparty/sys/VidMemViaD3D9.cpp
+++ src/3rdparty/sys/VidMemViaD3D9.cpp
@@ -14,7 +14,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <d3d9.h>
-
+#pragma comment(lib, "d3d9.lib")
 
 //-----------------------------------------------------------------------------
 // Defines, and constants
