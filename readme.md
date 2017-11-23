44ba13dd34ec0bf7718047653197d56c22684403
Linqing Feng <fenglinqing@gmail.com>
2017-11-23 11:59:03 +0900

    detect qt version

--- readme.md
+++ readme.md
@@ -1,16 +1,17 @@
 ## Installation
 ### macOS:
-* install xcode, homebrew, qt5, intel c composer (for mkl, ipp, tbb) and python3
+* install xcode, homebrew, qt5 (>= 5.9), intel c composer (for mkl, ipp, tbb) and python3
 * brew install unrar p7zip git cmake ninja
 
 ### Windows:
-* install visual studio, qt5, intel c composer (for mkl, ipp, tbb), python3, git
+* install visual studio, qt5 (>= 5.9), intel c composer (for mkl, ipp, tbb), python3, git
 
 ### Linux:
-* install qt5, mkl, ipp, tbb, python3 and ninja
+* install qt5 (>= 5.9), mkl, ipp, tbb, python3 and ninja
 * sudo apt install unrar p7zip-full git nasm libglfw3-dev zlib1g-dev patchelf
 
 ### All:
+* run "python3 util/common_dirs.py" once or after updating Qt to create "QtInfo.cmake" file
 * code repository folder should be named as "atlas"; "atlas_others" folder and "atlas" folder should be in same directory
 * (setup SSH credential for github), run "python3 util/build_ext_libs.py all" to build external libraries
 * run "python3 util/build_and_deploy_atlas.py all" or build CMakeLists.txt
--- util/common_dirs.py
+++ util/common_dirs.py
@@ -1,54 +1,55 @@
 import os
 import sys
+import xml.etree.ElementTree as etree
 
 
-def curr_dir():
+def curr_dir() -> str:
     return os.path.abspath(os.path.dirname(__file__))
 
 
-def repository_dir():
+def repository_dir() -> str:
     res = os.path.normpath(os.path.join(curr_dir(), '..'))
     assert os.path.exists(res)
     return res
 
 
-def base_dir():
+def base_dir() -> str:
     res = os.path.normpath(os.path.join(repository_dir(), '..'))
     assert os.path.exists(res)
     return res
 
 
-def src_dir():
+def src_dir() -> str:
     res = os.path.join(repository_dir(), 'src')
     assert os.path.exists(res)
     return res
 
 
-def ext_dir():
+def ext_dir() -> str:
     res = os.path.join(src_dir(), '3rdparty')
     assert os.path.exists(res)
     return res
 
 
-def atlas_dir():
+def atlas_dir() -> str:
     res = os.path.join(src_dir(), 'atlas')
     assert os.path.exists(res)
     return res
 
 
-def img_dir():
+def img_dir() -> str:
     res = os.path.join(src_dir(), 'img')
     assert os.path.exists(res)
     return res
 
 
-def resource_dir():
+def resource_dir() -> str:
     res = os.path.join(atlas_dir(), 'Resources')
     assert os.path.exists(res)
     return res
 
 
-def src_package_dir():
+def src_package_dir() -> str:
     res = os.path.join(base_dir(), 'atlas_others')
     if not os.path.exists(res):
         if sys.platform.startswith('win'):
@@ -59,7 +60,7 @@ def src_package_dir():
     return res
 
 
-def build_dir():
+def build_dir() -> str:
     res = os.path.join(repository_dir(), 'cmake-build-release')
     if not os.path.exists(res):
         os.mkdir(res)
@@ -67,7 +68,7 @@ def build_dir():
     return res
 
 
-def binary_dir():
+def binary_dir() -> str:
     if sys.platform.startswith('win32'):
         res = os.path.join(build_dir(), 'src', 'atlas', 'Release')
     else:
@@ -76,23 +77,50 @@ def binary_dir():
     return res
 
 
-def deploy_target_dir():
+def deploy_target_dir() -> str:
     return repository_dir()
 
 
-def qt_ver():
-    return '5.9.2'
-
-
-def qt_base_dir():
+def qt_install_dir() -> str:
     if sys.platform.startswith('win32'):
-        res = os.path.join('C:', os.sep, 'Qt', 'Qt', qt_ver(), 'msvc2017_64')
+        res = os.path.join('C:', os.sep, 'Qt', 'Qt')
     elif sys.platform.startswith('darwin'):
-        res = os.path.join(os.path.expanduser('~'), 'Qt', qt_ver(), 'clang_64')
+        res = os.path.join(os.path.expanduser('~'), 'Qt')
     else:
-        res = os.path.join(os.path.expanduser('~'), 'Qt', qt_ver(), 'gcc_64')
+        res = os.path.join(os.path.expanduser('~'), 'Qt')
     assert os.path.exists(res)
     return res
+
+
+def qt_ver() -> str:
+    component_file = os.path.join(qt_install_dir(), 'components.xml')
+    assert os.path.exists(component_file)
+    tree = etree.parse(component_file)
+    root = tree.getroot()
+    ver = None
+    for child in root:
+        if child.tag == 'ApplicationName' and child.text.startswith('Qt '):
+            ver = child.text[3:]
+    assert ver is not None
+    return ver
+
+
+def qt_base_dir() -> str:
+    if sys.platform.startswith('win32'):
+        res = os.path.join(qt_install_dir(), qt_ver(), 'msvc2017_64')
+    elif sys.platform.startswith('darwin'):
+        res = os.path.join(qt_install_dir(), qt_ver(), 'clang_64')
+    else:
+        res = os.path.join(qt_install_dir(), qt_ver(), 'gcc_64')
+    assert os.path.exists(res)
+    return res
+
+
+def write_cmake_file_with_qt_info():
+    with open(os.path.join(repository_dir(), 'cmake', 'QtInfo.cmake'), mode='w', encoding='utf-8') as f:
+        f.write('# Set Qt related variables\n')
+        f.write(f'set(QT_VERSION {qt_ver()})\n')
+        f.write(f'set(QT_PATHS {qt_base_dir()})\n')
 
 
 def software_dir() -> str:
@@ -103,7 +131,12 @@ def software_dir() -> str:
     return res
 
 
-def qt_bin_dir():
+def qt_bin_dir() -> str:
     res = os.path.join(qt_base_dir(), 'bin')
     assert os.path.exists(res)
     return res
+
+
+if __name__ == "__main__":
+    print(f'Qt {qt_ver()} in {qt_base_dir()}')
+    write_cmake_file_with_qt_info()
