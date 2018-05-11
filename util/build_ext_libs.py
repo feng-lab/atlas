1cb4c29961d00ec643addedfac7fc3d56ac0e8e7
Linqing Feng <fenglinqing@gmail.com>
2018-05-11 15:26:35 +0900

    linux mex

--- util/build_ext_libs.py
+++ util/build_ext_libs.py
@@ -202,7 +202,7 @@ def get_cmake_cmd_common_part(install_dir: str):
         res = [get_cmake_binary(),  # '-E', 'echo',
                '-DCMAKE_BUILD_TYPE=Release',
                '-DCMAKE_INSTALL_PREFIX=' + install_dir,
-               '-DCMAKE_C_FLAGS:STRING=-fPIC',
+               '-DCMAKE_C_FLAGS:STRING=-fPIC'
                '-DCMAKE_CXX_FLAGS:STRING=-std=c++14 -fPIC'
                ]
         if common_dirs.use_ninja():
@@ -404,7 +404,8 @@ def build_libjpeg(src_dir: str, install_dir: str, ext_dir: str, nasm_dir: str):
             if is_linux():
                 subprocess.run(['sh', src_dir + '/configure',
                                 'NASM=nasm',
-                                '--enable-static', '--disable-shared'],
+                                '--enable-static', '--disable-shared',
+                                'CFLAGS=-fPIC'],
                                cwd=build_dir, shell=False, check=True)
             else:
                 subprocess.run(['sh', src_dir + '/configure', '--host', 'x86_64-apple-darwin',
