d31841b58dbf75fada9eec7d5c3f1112559effe6
Linqing Feng <fenglinqing@gmail.com>
2018-12-14 16:31:43 +0900

    ..

--- src/atlas/z3dimg.cpp
+++ src/atlas/z3dimg.cpp
@@ -49,8 +49,14 @@ Z3DImg::Z3DImg(const ZImgPack& imgPack, const glm::vec3& scale, QObject* parent)
     m_pageTableBlockSize = glm::uvec3(32, 32, 32);
     m_imageBlockSize = imageBlockSize();
     m_imageBlockReadSize = glm::ivec3(510, 510, 30);
-    if (Z3DGpuInfo::instance().dedicatedVideoMemoryMB() >= 4096) {
-      m_imageCacheNumBlocks = glm::uvec3(64, 32, 32); // 1G
+    if (Z3DGpuInfo::instance().dedicatedVideoMemoryMB() >= 16384) {
+      m_imageCacheNumBlocks = glm::uvec3(64, 64, 64); // 8G
+      m_pageTableCacheNumBlocks = glm::uvec3(8, 8, 8); // 256*256*256*4*4   256MB
+    } else if (Z3DGpuInfo::instance().dedicatedVideoMemoryMB() >= 8192) {
+      m_imageCacheNumBlocks = glm::uvec3(64, 64, 48); // 6G
+      m_pageTableCacheNumBlocks = glm::uvec3(8, 8, 6); // 256*256*192*4*4   192MB
+    } else if (Z3DGpuInfo::instance().dedicatedVideoMemoryMB() >= 4096) {
+      m_imageCacheNumBlocks = glm::uvec3(64, 32, 32); // 2G
       m_pageTableCacheNumBlocks = glm::uvec3(8, 8, 2); // 256*256*64*4*4   64MB
     } else if (Z3DGpuInfo::instance().dedicatedVideoMemoryMB() >= 2048) {
       m_imageCacheNumBlocks = glm::uvec3(32, 32, 16);
