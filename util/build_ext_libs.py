e60b21b1988f9d1073af825dda2a32e9df067c12
Linqing Feng <fenglinqing@gmail.com>
2019-06-05 19:57:17 +0900

    uplib

--- util/build_ext_libs.py
+++ util/build_ext_libs.py
@@ -1031,7 +1031,7 @@ def build_opencv(src_dir: str, src_contrib_dir: str, install_dir: str):
                          '-DBUILD_opencv_dnn_objdetect:BOOL=OFF',
                          '-DBUILD_opencv_world:BOOL=OFF',
                          '-DBUILD_opencv_python2:BOOL=OFF',
-                         '-DBUILD_opencv_python3:BOOL=ON',
+                         '-DBUILD_opencv_python3:BOOL=OFF',
                          '-DPYTHON3_EXECUTABLE=' + os.environ['CONDA_PYTHON_EXE'],
                          '-DINSTALL_PYTHON_EXAMPLES:BOOL=ON',
                          '-DOPENCV_ENABLE_NONFREE:BOOL=ON',
