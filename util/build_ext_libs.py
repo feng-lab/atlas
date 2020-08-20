44ff6fe60fcce0238463828c3c61181f55c7cc33
Linqing Feng <fenglinqing@gmail.com>
2020-08-20 18:26:15 +0900

    try fix glbinding

--- util/build_ext_libs.py
+++ util/build_ext_libs.py
@@ -887,7 +887,9 @@ def build_glbinding(src_dir: str, install_dir: str):
         cmakecmd.extend(['-DOPTION_BUILD_TOOLS:BOOL=OFF',
                          '-DBUILD_SHARED_LIBS:BOOL=OFF',
                          '-DOPTION_BUILD_TESTS:BOOL=OFF',
+                         '-DOPTION_BUILD_DOCS:BOOL=OFF',
                          '-DOPTION_BUILD_EXAMPLES:BOOL=OFF',
+                         '-DOPTION_BUILD_OWN_KHR_HEADERS:BOOL=ON',
                          src_dir])
         build_and_install_cmakecmd(cmakecmd, build_dir)
     finally:
