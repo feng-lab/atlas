15c5233537fd1cc894f1b8ff29c50b9174759379
Linqing Feng <fenglinqing@gmail.com>
2021-12-23 15:54:16 +0800

    update maintenance tool version to 4.2.0

--- util/deploy_atlas.py
+++ util/deploy_atlas.py
@@ -25,7 +25,7 @@ def update_pacakge_xml_version(template_file: str, file: str):
 
 def update_maintenance_pacakge_xml_version(template_file: str, file: str):
     tree = eTree.parse(template_file)
-    tree.find('Version').text = '4.1.1'  # todo: get version and date from qt components.xml
+    tree.find('Version').text = '4.2.0'  # todo: get version and date from qt components.xml
     tree.find('ReleaseDate').text = '2021-05-25'
     # Write back to file
     tree.write(file, encoding="utf-8", xml_declaration=True)
