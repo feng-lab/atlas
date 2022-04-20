6d0338efe7e09dface8fd7b656d4ff2d9ab839f1
Linqing Feng <fenglinqing@gmail.com>
2022-04-20 15:38:58 +0800

    fix build

--- util/all.sh
+++ util/all.sh
@@ -4,9 +4,9 @@ cd $(pwd)
 cd ..
 
 conda activate pt
-python utils/build_ext_libs.py all
-python utils/build_and_deploy_atlas.py
+python util/build_ext_libs.py all
+python util/build_and_deploy_atlas.py
 
-python utils/build_ext_libs.py conda-zimg
+python util/build_ext_libs.py conda-zimg
 conda-build conda-opencv-recipe
 conda-build zimg-recipe
