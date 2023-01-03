46c95cc1e7bb384ef8ce50cd250071661bd8802a
Linqing Feng <fenglinqing@gmail.com>
2023-01-03 16:47:48 +0800

    remove geom shader

--- src/atlas/z3dlinerenderer.cpp
+++ src/atlas/z3dlinerenderer.cpp
@@ -33,7 +33,7 @@ Z3DLineRenderer::Z3DLineRenderer(Z3DRendererBase& rendererBase)
   , m_VAOs(1)
   , m_pickingVAOs(1)
   , m_oneBatchNumber(4e6)
-  , m_useGeomLineShader(GLVersionGE(3, 2))
+  , m_useGeomLineShader(false)  // GLVersionGE(3, 2)
 {
   updateLineWidth();
   connect(&m_rendererBase.geometriesMultisampleModePara(),
