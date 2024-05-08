2cbf979bbd9f0a95018edca495238def5bf1c936
Linqing Feng <fenglinqing@gmail.com>
2024-05-08 17:38:58 +0800

    fix absl log and glog conflict

--- src/atlas/zcustomcommand.cpp
+++ src/atlas/zcustomcommand.cpp
@@ -26,13 +26,11 @@
 #include "z3dmeshview.h"
 #include "z3dpunctaview.h"
 #include "z3dswcview.h"
-#include "helloworld.grpc.pb.h"
 #include <qtcsv/reader.h>
 #include <itkMath.h>
 #include <QDir>
 #include <QApplication>
 #include <tbb/global_control.h>
-#include <grpcpp/grpcpp.h>
 
 namespace nim {
 
@@ -1677,70 +1675,6 @@ void testDetectPuncta()
   pd2.run();
 
   LOG(WARNING) << "abc";
-}
-
-using grpc::Channel;
-using grpc::ChannelArguments;
-using grpc::ClientContext;
-using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
-
-class GreeterClient
-{
-public:
-  explicit GreeterClient(std::shared_ptr<Channel> channel)
-    : m_stub(Greeter::NewStub(channel))
-  {}
-
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user)
-  {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
-
-    // Container for the data we expect from the server.
-    HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // Overwrite the call's compression algorithm to DEFLATE.
-    context.set_compression_algorithm(GRPC_COMPRESS_DEFLATE);
-
-    // The actual RPC.
-    Status status = m_stub->SayHello(&context, request, &reply);
-
-    // Act upon its status.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-      return "RPC failed";
-    }
-  }
-
-private:
-  std::unique_ptr<Greeter::Stub> m_stub;
-};
-
-void helloworldrpc()
-{
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  ChannelArguments args;
-  // Set the default compression algorithm for the channel.
-  args.SetCompressionAlgorithm(GRPC_COMPRESS_GZIP);
-  GreeterClient greeter(grpc::CreateCustomChannel("161.122.102.55:50051", grpc::InsecureChannelCredentials(), args));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);
-  LOG(INFO) << "Greeter received: " << reply;
 }
 
 void cutZeroRegion()
