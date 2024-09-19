54a3c08f53402ddb2fdb1e21c202e8913d14e4bd
Linqing Feng <fenglinqing@gmail.com>
2024-09-19 11:20:08 +0800

    remove deprecated strerror

--- src/img/zexception.h
+++ src/img/zexception.h
@@ -2,8 +2,7 @@
 
 #include <QString>
 #include <fmt/base.h>
-#include <cerrno>
-#include <cstring>
+#include <system_error>
 #include <stdexcept>
 #include <string>
 
@@ -28,7 +27,7 @@ public:
     : m_what(std::move(what))
   {
     if (option == Option::CheckErrno && errno != 0) {
-      fmt::format_to(std::back_inserter(m_what), "  <errno: {}>", std::strerror(errno));
+      fmt::format_to(std::back_inserter(m_what), "  <errno: {}>", std::make_error_code(std::errc(errno)).message());
       errno = 0;
     }
   }
@@ -46,7 +45,7 @@ public:
     auto u8 = what.toUtf8();
     std::string(u8.data(), u8.size()).swap(m_what);
     if (option == Option::CheckErrno && errno != 0) {
-      fmt::format_to(std::back_inserter(m_what), "  <errno: {}>", std::strerror(errno));
+      fmt::format_to(std::back_inserter(m_what), "  <errno: {}>", std::make_error_code(std::errc(errno)).message());
       errno = 0;
     }
   }
