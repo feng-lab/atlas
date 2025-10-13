b2e02221f2678df478d48fd7ec0ffa64b3799354
Linqing Feng <fenglinqing@gmail.com>
2025-10-13 19:14:37 +0800

    revert blend state to match gl

--- src/atlas/zvulkantextureblendpipelinecontext.cpp
+++ src/atlas/zvulkantextureblendpipelinecontext.cpp
@@ -242,12 +242,9 @@ ZVulkanTextureBlendPipelineContext::ensurePipeline(const PipelineKey& key, const
   instance.pipeline->setAttachmentFormats(formats.colorFormats, formats.depthFormat);
   instance.pipeline->setCullMode(vk::CullModeFlagBits::eNone);
   instance.pipeline->setFrontFace(vk::FrontFace::eCounterClockwise);
-  const bool hasDepth = formats.depthFormat.has_value();
-  instance.pipeline->setDepthTestEnable(hasDepth);
-  instance.pipeline->setDepthWriteEnable(hasDepth);
-  if (hasDepth) {
-    instance.pipeline->setDepthCompareOp(vk::CompareOp::eLessOrEqual);
-  }
+  instance.pipeline->setDepthTestEnable(true);
+  instance.pipeline->setDepthCompareOp(vk::CompareOp::eAlways);
+  instance.pipeline->setDepthWriteEnable(true);
 
   // Final compositor pass needs premultiplied alpha blending over the
   // already-rendered background, matching the GL path.
