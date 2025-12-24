548c8a33f1161897a5585520a84c06d526b2bfac
Linqing Feng <fenglinqing@gmail.com>
2025-12-24 10:49:27 +0800

    bump deploy versions

--- util/deploy_atlas.py
+++ util/deploy_atlas.py
@@ -998,8 +998,10 @@ def update_pacakge_xml_version(template_file: str, file: str):
 
 def update_maintenance_pacakge_xml_version(template_file: str, file: str):
     tree = eTree.parse(template_file)
-    tree.find('Version').text = '4.7.0'  # todo: get version and date from qt components.xml
-    tree.find('ReleaseDate').text = '2024-02-15'
+    tree.find(
+        "Version"
+    ).text = "4.7.1"  # todo: get version and date from qt components.xml
+    tree.find("ReleaseDate").text = "2024-02-16"
     # Write back to file
     tree.write(file, encoding="utf-8", xml_declaration=True)
 
